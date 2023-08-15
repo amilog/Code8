89925119f1be77fdbc8c7f731ff38dff1fb8fdd8
Babayev03 <ilhambabayev222@gmail.com>
2023-08-15 11:33:19 -0700

    paw nomre 2

--- src/screens/Onboarding.jsx
+++ src/screens/Onboarding.jsx
@@ -1,30 +1,37 @@
-import React from 'react';
-import { View, StyleSheet, Text, ImageBackground, StatusBar } from 'react-native';
+import React from "react";
+import {
+  View,
+  StyleSheet,
+  Text,
+  ImageBackground,
+  StatusBar,
+} from "react-native";
 
 const Onboarding = () => {
-    return (
-        <>
-            <StatusBar translucent backgroundColor="transparent" />
-            <ImageBackground
-                source={require('../assets/images/OnboardingImg1.jpg')}
-                style={styles.imageBackground}>
-                <View>
-                    <Text>PAW</Text>
-                </View>
-            </ImageBackground>
-        </>
-    );
-}
+  return (
+    <>
+      <StatusBar translucent backgroundColor="transparent" />
+      <ImageBackground
+        source={require("../assets/images/OnboardingImg1.jpg")}
+        style={styles.imageBackground}
+      >
+        <View>
+          <Text style={{ color: "red" }}>PAW</Text>
+        </View>
+      </ImageBackground>
+    </>
+  );
+};
 
 const styles = StyleSheet.create({
-    imageBackground: {
-        flex: 1,
-        resizeMode: 'cover',
-        justifyContent: 'center',
-        alignItems: 'center',
-        width: '100%',
-        height: '100%',
-    },
-})
+  imageBackground: {
+    flex: 1,
+    resizeMode: "cover",
+    justifyContent: "center",
+    alignItems: "center",
+    width: "100%",
+    height: "100%",
+  },
+});
 
 export default Onboarding;
