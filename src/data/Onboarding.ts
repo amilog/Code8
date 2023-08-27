d1db523cb254001bd310267bc29f3d7f9c9557eb
amilgasimov <amillgasimov@gmail.com>
2023-08-27 14:10:18 +0400

    a

---
+++ src/data/Onboarding.ts
@@ -0,0 +1,7 @@
+export const Onboarding = [
+    {
+        title: 'Welcome to the Onboarding',
+        description: '/onboarding',
+        image: require('../assets/images/onboarding1.png'),
+    }
+]
---
+++ src/screens/Animation.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Animation = () => {
+    return (
+        <View>
+            <Text>Animation</Text>
+        </View>
+    )
+}
+
+export default Animation
+
+const styles = StyleSheet.create({})
