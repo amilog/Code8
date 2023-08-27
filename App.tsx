8c5d36e0cc1c79fe65542b1fe284f1e815bfb06a
amilgasimov <amillgasimov@gmail.com>
2023-08-27 13:12:33 +0400

    tst

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to ah working on your app!</Text>
+      <Text>Ilhamdan olaninki</Text>
       <StatusBar style="auto" />
     </View>
   );
