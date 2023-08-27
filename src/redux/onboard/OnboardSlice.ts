4b9faf39aa7f1a7da8f9619efe66150144d52263
İlham Babayev <ilhambabayev222@gmail.com>
2023-08-27 15:10:18 +0400

    Redux added

---
+++ src/redux/onboard/OnboardSlice.ts
@@ -0,0 +1,49 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+interface OnboardState {
+  isOnboarded: boolean;
+  loading: boolean;
+}
+
+const initialState: OnboardState = {
+  isOnboarded: false,
+  loading: false,
+};
+
+export const getStatus = createAsyncThunk("onboard/getStatus", async () => {
+  const response = await AsyncStorage.getItem("isOnboarded");
+  return response;
+});
+
+export const setStatus = createAsyncThunk(
+  "onboard/setStatus",
+  async (status: boolean) => {
+    await AsyncStorage.setItem("isOnboarded", status.toString());
+    return status;
+  }
+);
+
+const onboardSlice = createSlice({
+  name: "onboard",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(getStatus.pending, (state) => {
+      state.loading = true;
+    });
+    builder.addCase(getStatus.fulfilled, (state, action) => {
+      state.loading = false;
+      state.isOnboarded = action.payload === "true";
+    });
+    builder.addCase(setStatus.pending, (state) => {
+      state.loading = true;
+    });
+    builder.addCase(setStatus.fulfilled, (state, action) => {
+      state.loading = false;
+      state.isOnboarded = action.payload;
+    });
+  },
+});
+
+export const onboardReducer = onboardSlice.reducer;
