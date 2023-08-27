abd416d1ae469166d64688ef4248c8acbf57c93c
amilgasimov <amillgasimov@gmail.com>
2023-08-27 17:24:42 +0400

    ozume paw

--- App.tsx
+++ App.tsx
@@ -2,12 +2,14 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import Tab from "./src/stacks/Tab";
 import { NavigationContainer } from "@react-navigation/native";
+import Animation from "./src/screens/Animation";
 
 export default function App() {
   return (
     <NavigationContainer>
       <StatusBar style="auto" translucent />
-      <Tab />
+      {/* <Tab /> */}
+       <Animation/>
     </NavigationContainer>
   );
 }
