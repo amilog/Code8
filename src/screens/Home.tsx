02e282d9da23b0869e8cc9951c5bde6b58940db4
İlham Babayev <ilhambabayev222@gmail.com>
2023-08-28 17:25:56 +0400

    Team cards added

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -5,7 +5,7 @@ import {
   StyleSheet,
   Text,
   View,
-  Platform,
+  TouchableOpacity,
 } from "react-native";
 import React from "react";
 import GradientHeader from "../components/GradientHeader";
@@ -14,33 +14,43 @@ import { Teams } from "../data/Team";
 import { TeamsModel } from "../models/TeamsModel";
 
 const Home = () => {
+  const renderVerticalItem = ({ item }: { item: TeamsModel }) => {
+    return (
+      <TouchableOpacity style={styles.teamCard}>
+        <Image source={item.teamPic} style={styles.teamPic} />
+        <View style={styles.teamLabel}>
+          <Text style={styles.teamName}>{item.name}</Text>
+          <View style={styles.memberLabel}>
+            {item.members.slice(0, 3).map((member, index) => {
+              if (index === 2) {
+                return (
+                  <Text key={member._id} style={styles.memberName}>
+                    ...
+                  </Text>
+                );
+              }
+              return (
+                <Text key={member._id} style={styles.memberName}>
+                  {member.fullName}
+                  {", "}
+                </Text>
+              );
+            })}
+          </View>
+        </View>
+      </TouchableOpacity>
+    );
+  };
+
   return (
     <View style={styles.container}>
       <GradientHeader title={"Layihe"} />
       <FlatList
         data={Teams}
-        showsHorizontalScrollIndicator={false}
+        showsVerticalScrollIndicator={false}
         keyExtractor={(item) => item._id}
-        contentContainerStyle={{
-          paddingBottom: 20,
-          paddingTop: 20,
-          flexGrow: 1,
-        }}
-        renderItem={({ item }: { item: TeamsModel }) => {
-          return (
-            <View style={styles.teamCard}>
-              <Image
-                source={item.teamPic}
-                style={{
-                  width: 75,
-                  height: 75,
-                  resizeMode: "contain",
-                  borderRadius: 38,
-                }}
-              />
-            </View>
-          );
-        }}
+        contentContainerStyle={styles.contentContainerStyle}
+        renderItem={renderVerticalItem}
       />
     </View>
   );
@@ -57,12 +67,50 @@ const styles = StyleSheet.create({
   teamCard: {
     width: "95%",
     height: Dimensions.get("window").height / 7,
-    flexDirection: "row",
-    padding: 16,
-    borderWidth: 1,
-    alignSelf: "center",
-    marginBottom: 10,
+    backgroundColor: "#fff",
     borderRadius: 16,
+    alignSelf: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 10,
+    flexDirection: "row",
+    marginBottom: 10,
+    alignItems: "center",
+    padding: 16,
+  },
+
+  teamName: {
+    fontSize: 22,
+    fontWeight: "bold",
+    color: "#000",
+  },
+  memberName: {
+    fontSize: 16,
+    color: "#868686",
+  },
+  teamPic: {
+    width: 75,
+    height: 75,
+    resizeMode: "contain",
+    borderRadius: 38,
+  },
+  teamLabel: {
+    height: 69,
+    marginLeft: 16,
+    gap: 6,
+  },
+  contentContainerStyle: {
+    paddingBottom: 20,
+    paddingTop: 20,
+    flexGrow: 1,
+  },
+  memberLabel: {
+    flexDirection: "row",
     alignItems: "center",
   },
 });
