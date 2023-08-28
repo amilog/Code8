409a373f4b9e6228696af5b650b7ac61d82e351c
amilgasimov <amillgasimov@gmail.com>
2023-08-28 15:27:21 +0400

    fixed: src/screens/Home.tsx

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,14 +1,66 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import {
+  Dimensions,
+  FlatList,
+  Image,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
+import React from "react";
+import GradientHeader from "../components/GradientHeader";
+import { StatusBar } from "react-native";
+import { Teams } from "../data/Team";
+import { TeamsModel } from "../models/TeamsModel";
 
 const Home = () => {
-    return (
-        <View>
-            <Text>Home</Text>
-        </View>
-    )
-}
+  return (
+    <View style={styles.container}>
+      <GradientHeader title={"Layihe"} />
+      <FlatList
+        data={Teams}
+        showsHorizontalScrollIndicator={false}
+        keyExtractor={(item) => item._id}
+        contentContainerStyle={{
+          paddingBottom: 20,
+          paddingTop: 20,
+          flexGrow: 1,
+        }}
 
-export default Home
+        renderItem={({ item }: { item: TeamsModel }) => {
+          return (
+            <View style={styles.teamCard}>
+              <Image
+                source={item.teamPic}
+                style={{
+                  width: '24%',
+                  height: '100%',
+                  resizeMode: "contain",
+                  borderRadius: Dimensions.get("window").height / 2
+                }}
+              />
+            </View>
+          );
+        }}
+      />
+    </View>
+  );
+};
 
-const styles = StyleSheet.create({})
+export default Home;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#ffffff",
+    paddingTop: StatusBar.currentHeight! + 20,
+  },
+  teamCard: {
+    width: "85%",
+    height: Dimensions.get("window").height / 8,
+    // height: 96
+    flexDirection: "row",
+    padding:16,
+    borderWidth: 1,
+    alignSelf: "center",
+  },
+});
