75cb9ede983abc22d55e636f653e10df41ab38ee
amilgasimov <amillgasimov@gmail.com>
2023-08-28 17:09:01 +0400

    code text add

--- App.tsx
+++ App.tsx
@@ -8,8 +8,8 @@ export default function App() {
   return (
     <NavigationContainer>
       <StatusBar style="auto" translucent />
-      <Tab />
-       {/* <Animation/> */}
+      {/* <Tab /> */}
+       <Animation/>
     </NavigationContainer>
   );
 }
