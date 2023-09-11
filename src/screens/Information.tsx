9f8af300cc743dd44e73cfacc812d54d436c7723
İlham Babayev <ilhambabayev222@gmail.com>
2023-09-11 17:33:14 +0400

    appp updated

--- src/screens/Information.tsx
+++ src/screens/Information.tsx
@@ -1,34 +1,13 @@
-import React, { useState } from "react";
-import { View, StyleSheet, StatusBar } from "react-native";
+import React from "react";
+import { View, StyleSheet, StatusBar, Platform } from "react-native";
 import GradientHeader from "../components/GradientHeader";
-import LastHackathon from "./LastHackathon";
-import AdvanceCareer from "./AdvanceCareer";
-import HorizontalTabBar from "../components/TopNavigation";
+import InfoTopNavigation from "../stacks/InfoTopNavigaiton";
 
 const Information = () => {
-  const [focusedItem, setFocusedItem] = useState("Son hackathon");
-
-  const tabs = ["Son hackathon", "Karyerani yukselt"];
-
-  const renderScreen = () => {
-    if (focusedItem === "Son hackathon") {
-      return <LastHackathon />;
-    } else {
-      return <AdvanceCareer />;
-    }
-  };
-
   return (
     <View style={styles.container}>
-      <GradientHeader title={"Məlumat"} />
-      <View style={styles.tabContainer}>
-        <HorizontalTabBar
-          tabs={tabs}
-          focusedItem={focusedItem}
-          setFocusedItem={setFocusedItem}
-        />
-      </View>
-      {renderScreen()}
+      <GradientHeader title={"Məlumatlar"} />
+      <InfoTopNavigation />
     </View>
   );
 };
@@ -37,12 +16,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    paddingTop: StatusBar.currentHeight! + 20,
-  },
-  tabContainer: {
-    borderBottomWidth: 0.5,
-    height: 70,
-    borderBottomColor: "#F3D1FF",
+    paddingTop: Platform.OS == "ios" ? 40 : StatusBar.currentHeight! + 20,
   },
 });
 
--- src/screens/ValuationForm.tsx
+++ src/screens/ValuationForm.tsx
@@ -1,31 +1,12 @@
-import { StatusBar, StyleSheet, Text, View } from "react-native";
-import React, { useState } from "react";
+import { Platform, StatusBar, StyleSheet, Text, View } from "react-native";
+import React from "react";
 import GradientHeader from "../components/GradientHeader";
-import HorizontalTabBar from "../components/TopNavigation";
+import ValuationTopNavigation from "../stacks/ValuationTopNavigation";
 
 const ValutionForm = () => {
-  const [focusedItem, setFocusedItem] = useState("Qiymetlendirme");
-
-  const tabs = ["Qiymetlendirme", "Netice"];
-
-  const renderScreen = () => {
-    if (focusedItem === "Qiymetlendirme") {
-      return null;
-    } else {
-      return null;
-    }
-  };
   return (
     <View style={styles.container}>
       <GradientHeader title="Qiymetlendirme" />
-      <View style={styles.tabContainer}>
-        <HorizontalTabBar
-          tabs={tabs}
-          focusedItem={focusedItem}
-          setFocusedItem={setFocusedItem}
-        />
-      </View>
-      {renderScreen()}
     </View>
   );
 };
@@ -36,27 +17,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#ffffff",
-    paddingTop: StatusBar.currentHeight! + 20,
-  },
-  tabContainer: {
-    borderBottomWidth: 0.5,
-    height: 70,
-    borderBottomColor: "#F3D1FF",
-  },
-  tabList: {
-    marginTop: 10,
-  },
-  tab: {
-    height: 50,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  tabText: {
-    fontSize: 18,
-    fontWeight: "500",
-  },
-  focusedTabIcon: {
-    position: "absolute",
-    bottom: -9,
+    paddingTop: Platform.OS == "ios" ? 40 : StatusBar.currentHeight! + 20,
   },
 });
