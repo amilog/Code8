e0a8d04787d17041a680f4c41b4e4d76f4d55308
amilog <amillgasimov@gmail.com>
2023-09-28 00:19:32 +0400

    task screens completed

--- src/screens/task/Backend.tsx
+++ src/screens/task/Backend.tsx
@@ -1,11 +1,174 @@
-import { StyleSheet, Text, View, Platform, StatusBar } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Platform,
+  StatusBar,
+  ScrollView,
+} from "react-native";
 import React from "react";
 import GradientHeader from "../../components/GradientHeader";
+import Unorderedlist from "react-native-unordered-list";
 
 const Backend = ({ navigation }: any) => {
   return (
     <View style={styles.container}>
       <GradientHeader title="Back-End" showArrow navigation={navigation} />
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <View style={styles.textContainer}>
+          <Text style={styles.text}>1. Introduction (10 bal)</Text>
+          <View style={styles.steps}>
+            <Unorderedlist bulletUnicode={0x2022}>
+              <Text style={styles.text}>
+                İzahat: Proyekt 3 arxitexturadan biri ilə yazılmalıdır. Pattern
+                istifadə edilməlidir. (Clean, Onion, N-Tier, Repository,
+                Options, CQRS, UOW, SO, Mediator və s.). User Cədvəli: (ID,
+                Name, BirthDate, Gender, Email, Password, AppointmentDate,
+                DeviceId). Education Cədvəli: (ID, University Name, Speciality,
+                Current Grade Level, GPA). Interests Cədvəli: (ID, Enjoyed
+                Subjects, Hobbies, Career Envision(Business, Design, etc.).
+                Applicants Cədvəli: (ID, Name, Status). Admin Datası seed
+                olunmalıdır.
+              </Text>
+            </Unorderedlist>
+          </View>
+          <View style={{ height: 30 }} />
+          <Text style={styles.text}>
+            2. Cihaz Qeydiyyatı Endpointi (10 bal)
+          </Text>
+          <View style={styles.steps}>
+            <Unorderedlist bulletUnicode={0x2022}>
+              <Text style={styles.text}>
+                İzahat: /api/register-deviceadında POST endpointiniz olur. Gələn
+                DeviceID dəyəri ilə user yaradırsınız.
+              </Text>
+            </Unorderedlist>
+          </View>
+          <View style={{ height: 30 }} />
+          <Text style={styles.text}>3. Müraciətlər Endpointi (5 bal)</Text>
+          <View style={styles.steps}>
+            <Unorderedlist bulletUnicode={0x2022}>
+              <Text style={styles.text}>
+                İzahat: /api/applicantsadında GET endpointiniz olur. Gələn
+                DeviceID dəyərinə uyğun istifadəçinin müraciətlərini geri
+                qaytarır.
+              </Text>
+            </Unorderedlist>
+          </View>
+          <View style={{ height: 30 }} />
+          <Text style={styles.text}>4. Kursa Qoşul Endpointi (10 bal)</Text>
+          <View style={styles.steps}>
+            <Unorderedlist bulletUnicode={0x2022}>
+              <Text style={styles.text}>
+                İzahat: /api/join-course adında POST endpointiniz olur.
+                İstifadəçilərin şəxsi məlumatlarını, təhsil məlumatlarını,
+                marağı olan sahələri qəbul edir və verilənlər bazasına qeyd
+                edir. Data science qrupunun verdiyi endpoint-ə lazım olan
+                dataları parametr olaraq göndərir. İstifadəçiyə tövsiyə edilən
+                kurs haqqında ətraflı məlumatları qaytarır və email olaraq
+                istifadəçiyə göndərilir. Status pending olaraq dəyişir.
+              </Text>
+            </Unorderedlist>
+          </View>
+          <View style={{ height: 30 }} />
+          <Text style={styles.text}>
+            5. Kursa Müraciət Et Endpointi (10 bal)
+          </Text>
+          <View style={styles.steps}>
+            <Unorderedlist bulletUnicode={0x2022}>
+              <Text style={styles.text}>
+                İzahat: /api/apply-course adında POST endpointiniz olur.
+                İstifadəçinin ID dəyərinə uyğun status unaccepted olaraq
+                dəyişir.
+              </Text>
+            </Unorderedlist>
+          </View>
+          <View style={{ height: 30 }} />
+          <Text style={styles.text}>6. Görüş Təyin Et Endpointi (10 bal)</Text>
+          <View style={styles.steps}>
+            <Unorderedlist bulletUnicode={0x2022}>
+              <Text style={styles.text}>
+                İzahat: /api/make-appointment adında POST endpointiniz olur.
+                Görüş tarixi yazılır. İstifadəçinin ID dəyərinə uyğun statusu
+                unaccepted olaraq dəyişir.
+              </Text>
+            </Unorderedlist>
+          </View>
+          <View style={{ height: 30 }} />
+          <Text style={styles.text}>7. Admin Giriş Endpointi (10 bal)</Text>
+          <View style={styles.steps}>
+            <Unorderedlist bulletUnicode={0x2022}>
+              <Text style={styles.text}>
+                İzahat: /api/login adında POST endpointiniz olur. Email və
+                Password ilə giriş olunacaq. Geriyə JWT qaytarılacaq.
+              </Text>
+            </Unorderedlist>
+          </View>
+          <View style={{ height: 30 }} />
+          <Text style={styles.text}>8. İstifadəçilər Endpointi (5 bal)</Text>
+          <View style={styles.steps}>
+            <Unorderedlist bulletUnicode={0x2022}>
+              <Text style={styles.text}>
+                İzahat: /api/users adında GET endpointiniz olur. Admin
+                Dashboard-da istifadəçiləri geri qaytarır. Bu Endpoint Admin
+                tərəfindən
+              </Text>
+            </Unorderedlist>
+          </View>
+          <View style={{ height: 30 }} />
+          <Text style={styles.text}>9. İstəyi Qəbul Et Endpointi (10 bal)</Text>
+          <View style={styles.steps}>
+            <Unorderedlist bulletUnicode={0x2022}>
+              <Text style={styles.text}>
+                İzahat: /api/accept-request adında POST endpointiniz olur. Görüş
+                tarixi yazılır. İstifadəçinin ID-nə uyğun statusu accepted
+                olaraq dəyişilir və həmin tələbənin mailinə ‘’Təbirklər!
+                Müraciətiniz qəbul oldu. 08/31/2023 16:00 tarixində görüş təyin
+                olundu’’ bildirişi göndərilir. (əlavə olaraq push notification
+                olması üstünlükdür). Bu Endpoint Admin tərəfindən idarə olunur.
+              </Text>
+            </Unorderedlist>
+          </View>
+          <View style={{ height: 30 }} />
+          <Text style={styles.text}>10. İstəyi Rədd Et Endpointi (5 bal)</Text>
+          <View style={styles.steps}>
+            <Unorderedlist bulletUnicode={0x2022}>
+              <Text style={styles.text}>
+                İzahat: /api/reject-request adında POST endpointiniz olur.
+                İstifadəçinin ID-nə uyğun statusu rejected olaraq dəyişilir və
+                həmin tələbənin mailinə ‘’Təəssüflər olsun ki siz kursa qəbul
+                ola bilmədiniz’’ bildirişi göndərilir. (əlavə olaraq push
+                notification olması üstünlükdür). Bu Endpoint Admin tərəfindən
+                idarə olunur.
+              </Text>
+            </Unorderedlist>
+          </View>
+          <View style={{ height: 30 }} />
+          <Text style={styles.text}>
+            11. Görüş Tarixini Dəyiş Endpointi (5 bal)
+          </Text>
+          <View style={styles.steps}>
+            <Unorderedlist bulletUnicode={0x2022}>
+              <Text style={styles.text}>
+                İzahat: /api/edit-appointment-date adında PUT endpointiniz olur.
+                İstifadəçinin ID-nə uyğun görüş tarixi dəyişilir.
+              </Text>
+            </Unorderedlist>
+          </View>
+          <View style={{ height: 30 }} />
+          <Text style={styles.text}>
+            12. Azure Portala Publish Etmək (10 bal)
+          </Text>
+          <View style={styles.steps}>
+            <Unorderedlist bulletUnicode={0x2022}>
+              <Text style={styles.text}>
+                İzahat: Proyekt Azure-da Virtual Machine yaradılıb publish
+                olunmalıdır.
+              </Text>
+            </Unorderedlist>
+          </View>
+        </View>
+      </ScrollView>
     </View>
   );
 };
@@ -17,5 +180,19 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#ffffff",
     paddingTop: Platform.OS == "ios" ? 40 : StatusBar.currentHeight! + 20,
+  },
+  text: {
+    fontSize: 18,
+    fontWeight: "500",
+    color: "#000000",
+    lineHeight: 26,
+  },
+  textContainer: {
+    marginTop: 16,
+    marginHorizontal: 16,
+    marginBottom: 26,
+  },
+  steps: {
+    paddingHorizontal: 16,
   },
 });
--- src/screens/task/UiUx.tsx
+++ src/screens/task/UiUx.tsx
@@ -1,11 +1,154 @@
-import { StyleSheet, View, Platform, StatusBar } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Platform,
+  StatusBar,
+  ScrollView,
+} from "react-native";
 import React from "react";
 import GradientHeader from "../../components/GradientHeader";
+import Unorderedlist from "react-native-unordered-list";
 
 const UiUx = ({ navigation }: any) => {
   return (
     <View style={styles.container}>
       <GradientHeader title="UX/UI" showArrow navigation={navigation} />
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <View style={styles.textContainer}>
+          <Text style={styles.title}>Ediləcəklər: FormApp, Dashboard</Text>
+          <Text style={styles.text}>1. FormApp: (45 bal)</Text>
+          <View style={styles.steps}>
+            <Text style={styles.text}>
+              Screens: Logo screen, Login, Home (about form details: status,
+              date .etc), Profile settings, Visual Design
+            </Text>
+          </View>
+          <View style={{ height: 30 }} />
+          <Text style={styles.text}>2. Benchmark və Ideation:(10 bal)</Text>
+          <View style={styles.steps}>
+            <Unorderedlist bulletUnicode={0x2022}>
+              <Text style={styles.text}>
+                Hədəf auditoriyasını, ehtiyaclarını, üstünlüklərini və ağrı
+                nöqtələrini araşdırın. Güclü və zəif tərəfləri müəyyən etmək
+                üçün mövcud həlləri və ya oxşar tətbiqləri təhlil edin. Fərqli
+                istifadəçi axınlarını və interfeys tərtibatlarını nəzərə alaraq,
+                beyin fırtınası edin və çoxlu dizayn konsepsiyalarını çəkin.
+              </Text>
+            </Unorderedlist>
+            <Unorderedlist bulletUnicode={0x2022}>
+              <Text style={styles.text}>
+                Verilən user flow-a uyğunlaşan ideyalara üstünlük verin.
+              </Text>
+            </Unorderedlist>
+          </View>
+          <View style={{ height: 30 }} />
+          <Text style={styles.text}>3. Dashboard: (45 bal)</Text>
+          <View style={styles.steps}>
+            <Unorderedlist bulletUnicode={0x2022}>
+              <Text style={styles.text}>
+                Sizə verilən dataya uyğun admin panel hazırlayın
+              </Text>
+            </Unorderedlist>
+            <Unorderedlist bulletUnicode={0x2022}>
+              <Text style={styles.text}>
+                Bu mərhələdə göndərilən sorğunun nəticəsi qəbul ya da rədd
+                olaraq qeyd olunacaq.
+              </Text>
+            </Unorderedlist>
+            <Unorderedlist bulletUnicode={0x2022}>
+              <Text style={styles.text}>Screens: Login (10 bal)</Text>
+            </Unorderedlist>
+            <Unorderedlist bulletUnicode={0x2022}>
+              <Text style={styles.text}>
+                Admin panel (account names,status, date etc.) (35 bal)
+              </Text>
+            </Unorderedlist>
+          </View>
+          <View style={{ height: 30 }} />
+          <Text style={styles.text}>Prototyping (Optional):</Text>
+          <View style={styles.steps}>
+            <Unorderedlist bulletUnicode={0x2022}>
+              <Text style={styles.text}>
+                Ola bilsinki kamandanızdakı developerlər məhdud vaxt ərzində
+                kodlamanı tamamlaya bilməsin. Təqdimat zamanı bu kimi hadisələr
+                kamandanı uğursuz göstərir. Məsləhət görərdim ki, developerlər
+                kodlama edən zaman dizaynerlər də prototip üzərində işləsinlər.
+              </Text>
+            </Unorderedlist>
+          </View>
+          <View style={{ height: 30 }} />
+          <Text style={styles.text}>Təqdimatın hazırlanması: </Text>
+          <View style={styles.steps}>
+            <Unorderedlist bulletUnicode={0x2022}>
+              <Text style={styles.text}>
+                Problemi, həll yolunu, istifadəçi səyahətini və vizual dizaynı
+                vurğulayan qısa təqdimat hazırlayın. Dizayn qərarlarınızın
+                arxasında duran məntiqi izah etməyə hazır olun.
+              </Text>
+            </Unorderedlist>
+          </View>
+          <View style={{ height: 30 }} />
+          <Text style={styles.text}>
+            Hazırlanan product üçün aktiv olaraq dev-lərlə əlaqədə olun:{" "}
+          </Text>
+          <View style={styles.steps}>
+            <Unorderedlist bulletUnicode={0x2022}>
+              <Text style={styles.text}>
+                Dizaynı ardıcıllıq, aydınlıq və layihənin məqsədlərinə uyğunluğu
+                üçün nəzərdən keçirin. Test zamanı və ya komanda üzvlərindən
+                alınan rəy əsasında dəqiqləşdirmələr edin.
+              </Text>
+            </Unorderedlist>
+          </View>
+          <View style={{ height: 30 }} />
+          <Text style={styles.text}>User Testing (Optional): </Text>
+          <View style={styles.steps}>
+            <Unorderedlist bulletUnicode={0x2022}>
+              <Text style={styles.text}>
+                Vaxt imkan verirsə, prototip haqqında rəy toplamaq üçün
+                potensial istifadəçilər və ya komanda üzvləri ilə sürətli
+                istifadə testləri keçirin.
+              </Text>
+            </Unorderedlist>
+            <Unorderedlist bulletUnicode={0x2022}>
+              <Text style={styles.text}>
+                Hər hansı istifadə problemini müəyyənləşdirin və lazımi
+                düzəlişlər edin.
+              </Text>
+            </Unorderedlist>
+          </View>
+          <View style={{ height: 30 }} />
+          <Text style={styles.text}>Yekun təqdimat: </Text>
+          <View style={styles.steps}>
+            <Unorderedlist bulletUnicode={0x2022}>
+              <Text style={styles.text}>
+                Dizaynınızı hakaton zamanı münsiflərə və tamaşaçılara təqdim
+                edin.
+              </Text>
+            </Unorderedlist>
+            <Unorderedlist bulletUnicode={0x2022}>
+              <Text style={styles.text}>
+                Müraciət etdiyiniz problemi, tətbiq etdiyiniz istifadəçi
+                mərkəzli yanaşmanı və dizaynınızın problemi necə effektiv
+                şəkildə həll etdiyini aydın şəkildə bildirin.
+              </Text>
+            </Unorderedlist>
+            <Unorderedlist bulletUnicode={0x2022}>
+              <Text style={styles.text}>
+                Unutmayın ki, hakatonda vaxt məhduddur, ona görə də diqqəti
+                verilmiş vaxt çərçivəsində dəqiq müəyyən edilmiş və təsirli həll
+                yolu yaratmağa yönəldin. Komanda üzvləri ilə əməkdaşlıq və yol
+                boyu rəy toplamaq UX/UI dizaynınızın keyfiyyətini əhəmiyyətli
+                dərəcədə artıra bilər.
+              </Text>
+            </Unorderedlist>
+          </View>
+          <View style={{ height: 20 }} />
+          <Text style={styles.text}>Uğurlar!</Text>
+          <View style={{ height: 10 }} />
+        </View>
+      </ScrollView>
     </View>
   );
 };
@@ -17,5 +160,26 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#ffffff",
     paddingTop: Platform.OS == "ios" ? 40 : StatusBar.currentHeight! + 20,
+  },
+  text: {
+    fontSize: 18,
+    fontWeight: "500",
+    color: "#000000",
+    lineHeight: 26,
+  },
+  textContainer: {
+    marginTop: 16,
+    marginHorizontal: 16,
+    marginBottom: 26,
+  },
+  steps: {
+    paddingHorizontal: 16,
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: "700",
+    color: "#000000",
+    lineHeight: 26,
+    marginBottom: 16,
   },
 });
